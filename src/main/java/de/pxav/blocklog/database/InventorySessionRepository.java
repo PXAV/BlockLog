0f584b9e0f62b2205393fe838321fa69da9ba2ca
pxav <pxav.studios@gmail.com>
2020-12-21 19:39:26 +0100

    Add repository classes for inventory session classes

---
+++ src/main/java/de/pxav/blocklog/database/InventorySessionRepository.java
@@ -0,0 +1,39 @@
+package de.pxav.blocklog.database;
+
+import de.pxav.blocklog.model.InventorySession;
+import de.pxav.blocklog.model.SerialBlockLocation;
+import org.bukkit.entity.Player;
+import org.hibernate.SessionFactory;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * A class description goes here.
+ *
+ * @author pxav
+ */
+public abstract class InventorySessionRepository {
+
+  protected SessionFactory sessionFactory;
+
+  public InventorySessionRepository(SessionFactory sessionFactory) {
+    this.sessionFactory = sessionFactory;
+  }
+
+  public abstract InventorySession getById(int id);
+
+  public abstract List<InventorySession> getByPlayerUUID(UUID uuid);
+
+  public abstract List<InventorySession> getByLocation(SerialBlockLocation location);
+
+  public abstract void save(InventorySession session);
+
+  public abstract void delete(InventorySession session);
+
+  public Collection<InventorySession> getByPlayer(Player player) {
+    return getByPlayerUUID(player.getUniqueId());
+  }
+
+}
