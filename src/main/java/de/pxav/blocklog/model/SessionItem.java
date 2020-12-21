f4c42696086c383dce2a15825946f1bb5b43c04b
pxav <pxav.studios@gmail.com>
2020-12-21 19:59:07 +0100

    Add hibernate annotations to SessionItem

--- src/main/java/de/pxav/blocklog/model/SessionItem.java
+++ src/main/java/de/pxav/blocklog/model/SessionItem.java
@@ -1,16 +1,76 @@
 package de.pxav.blocklog.model;
 
+import de.pxav.blocklog.database.ItemDirectionConverter;
+import de.pxav.blocklog.database.MaterialConverter;
 import org.bukkit.Material;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
 
 /**
  * A class description goes here.
  *
  * @author pxav
  */
+@Entity
+@Table(name = "session_items")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 public class SessionItem {
 
+  @Id
+  @GeneratedValue(generator = "increment")
+  @GenericGenerator(name = "increment", strategy = "increment")
+  private Integer id = null;
+
+  @ManyToOne(fetch = FetchType.EAGER)
   private InventorySession session;
+
+  @Convert(converter = MaterialConverter.class)
   private Material material;
 
+  @Convert(converter = ItemDirectionConverter.class)
+  private ItemDirection itemDirection;
+
+  public SessionItem() {}
+
+  public SessionItem(InventorySession session, Material material, ItemDirection itemDirection) {
+    this.session = session;
+    this.material = material;
+    this.itemDirection = itemDirection;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public InventorySession getSession() {
+    return session;
+  }
+
+  public void setSession(InventorySession session) {
+    this.session = session;
+  }
+
+  public Material getMaterial() {
+    return material;
+  }
+
+  public void setMaterial(Material material) {
+    this.material = material;
+  }
+
+  public ItemDirection getItemDirection() {
+    return itemDirection;
+  }
+
+  public void setItemDirection(ItemDirection itemDirection) {
+    this.itemDirection = itemDirection;
+  }
 
 }
