49f2dd65fa7839b018366d85f0c48d262328b9e9
pxav <pxav.studios@gmail.com>
2020-12-21 23:32:11 +0100

    Add class converting bukkit inventory types

---
+++ src/main/java/de/pxav/blocklog/database/InventoryTypeConverter.java
@@ -0,0 +1,24 @@
+package de.pxav.blocklog.database;
+
+import org.bukkit.event.inventory.InventoryType;
+
+import javax.persistence.AttributeConverter;
+
+/**
+ * A class description goes here.
+ *
+ * @author pxav
+ */
+public class InventoryTypeConverter implements AttributeConverter<InventoryType, String> {
+
+  @Override
+  public String convertToDatabaseColumn(InventoryType inventoryType) {
+    return inventoryType.toString();
+  }
+
+  @Override
+  public InventoryType convertToEntityAttribute(String s) {
+    return InventoryType.valueOf(s);
+  }
+
+}
