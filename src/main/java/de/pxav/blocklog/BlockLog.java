d5bee85e14edc12742ca16ba352f5fdcb8d374cc
pxav <pxav.studios@gmail.com>
2020-12-21 15:05:59 +0100

    Initial commit

---
+++ src/main/java/de/pxav/blocklog/BlockLog.java
@@ -0,0 +1,11 @@
+package de.pxav.blocklog;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * A class description goes here.
+ *
+ * @author pxav
+ */
+public class BlockLog extends JavaPlugin {
+}
