851865462d4672bdbd6b6fa45915217722b7597f
pxav <pxav.studios@gmail.com>
2020-12-21 19:37:31 +0100

    Add class saving an item which has been moved during a session

---
+++ src/main/java/de/pxav/blocklog/model/SessionItem.java
@@ -0,0 +1,16 @@
+package de.pxav.blocklog.model;
+
+import org.bukkit.Material;
+
+/**
+ * A class description goes here.
+ *
+ * @author pxav
+ */
+public class SessionItem {
+
+  private InventorySession session;
+  private Material material;
+
+
+}
