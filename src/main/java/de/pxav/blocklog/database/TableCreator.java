348e318253560b106a087e46f4edc86437717359
pxav <pxav.studios@gmail.com>
2020-12-22 14:45:36 +0100

    Adjust SQL syntax in TableCreator to be SQLite compatible as well

--- src/main/java/de/pxav/blocklog/database/TableCreator.java
+++ src/main/java/de/pxav/blocklog/database/TableCreator.java
@@ -2,6 +2,7 @@ package de.pxav.blocklog.database;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -23,27 +24,33 @@ public class TableCreator {
 
   public void createSessionTable() {
     try (Session session = this.sessionFactory.openSession()) {
+      Transaction transaction = session.beginTransaction();
+
       session.createSQLQuery("CREATE TABLE IF NOT EXISTS inventory_sessions (" +
-              "id INT PRIMARY KEY AUTO_INCREMENT," +
+              "id INT PRIMARY KEY," +
               "playerUUID VARCHAR(50)," +
               "playerName VARCHAR(20)," +
               "blockLocation VARCHAR(30)," +
               "inventoryType VARCHAR(30)" +
               ")")
               .executeUpdate();
-    }
-  }
 
-  public void createItemTable() {
-    try (Session session = this.sessionFactory.openSession()) {
       session.createSQLQuery("CREATE TABLE IF NOT EXISTS session_items (" +
-              "id INT PRIMARY KEY AUTO_INCREMENT," +
+              "id INT PRIMARY KEY," +
               "session_id INT," +
               "material VARCHAR(20)," +
               "amount INT," +
               "itemDirection VARCHAR(15)" +
               ")")
               .executeUpdate();
+
+      transaction.commit();
+    }
+  }
+
+  public void createItemTable() {
+    try (Session session = this.sessionFactory.openSession()) {
+
     }
   }
 
