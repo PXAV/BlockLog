5d0a1fe5ee4d68bf3c24aaba34b6e6d9feddab16
pxav <pxav.studios@gmail.com>
2020-12-22 14:59:24 +0100

    Load configs and tables on plugin startup

--- src/main/java/de/pxav/blocklog/BlockLog.java
+++ src/main/java/de/pxav/blocklog/BlockLog.java
@@ -2,9 +2,24 @@ package de.pxav.blocklog;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import de.pxav.blocklog.config.BlockLogConfiguration;
+import de.pxav.blocklog.config.MessageConfiguration;
+import de.pxav.blocklog.config.SettingsConfiguration;
 import de.pxav.blocklog.database.CredentialsFile;
+import de.pxav.blocklog.database.InventorySessionRepository;
+import de.pxav.blocklog.database.TableCreator;
 import de.pxav.blocklog.inject.SimpleBinderModule;
+import de.pxav.blocklog.model.InventorySession;
+import de.pxav.blocklog.model.ItemDirection;
+import de.pxav.blocklog.model.SerialBlockLocation;
+import de.pxav.blocklog.model.SessionItem;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.hibernate.SessionFactory;
+
+import java.util.UUID;
 
 /**
  * A class description goes here.
@@ -24,6 +39,16 @@ public class BlockLog extends JavaPlugin {
     credentialsFile.createFile("plugins//BlockLog", "databaseCredentials.yml");
     credentialsFile.loadToCache();
 
+    BlockLogConfiguration settingsConfig = injector.getInstance(SettingsConfiguration.class);
+    settingsConfig.createFileIfNotExists();
+    settingsConfig.cacheConfig();
+
+    BlockLogConfiguration messagesConfig = injector.getInstance(MessageConfiguration.class);
+    messagesConfig.createFileIfNotExists();
+    messagesConfig.cacheConfig();
+
+    TableCreator tableCreator = injector.getInstance(TableCreator.class);
+    tableCreator.createTables();
   }
 
   @Override
